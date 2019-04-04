a9bbf69b1e47fba5288179433db1422990f9b714
Jasterix C <loune.calixte@gmail.com>
2019-04-04 18:27:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
 numbers = Array (2...number)
 arr = []
   numbers.each { |x| arr << x if number%x==0}
-    if arr.size <= 1 || x<0
+    if arr.size <= 1 || number<0
       true
     else
        false
